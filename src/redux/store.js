0209589c314f03d6d92a2f98aec9ac369238172c
MikhailZhelnin <zhelnin.mikhai@gmail.com>
2021-04-01 13:11:36 +0300

    add localStorage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,22 @@
 import { createStore } from 'redux';
 import rootReducer from './reducers/index';
 
+import { loadState, saveState } from '../localStorage';
+
+const persistedState = loadState();
+
 const store = createStore(
   rootReducer,
+  persistedState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
+
+store.subscribe(() => {
+  saveState({
+    tasks: store.getState().tasks,
+    theme: store.getState().theme,
+    filter: store.getState().filter,
+  });
+});
 
 export default store;
